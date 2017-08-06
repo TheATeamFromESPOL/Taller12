8a79a500f1b6078749f9473c4ec4e092d6878235
Luis Arízaga <luis.1142@hotmail.com>
2017-08-06 00:09:41 -0500

    ..

--- buscar.c
+++ buscar.c
@@ -7,7 +7,8 @@
 
 int palabraEnArreglo(char **arreglo,char *palabra);
 int numero_lineas(char *ruta, int *tam_lineas);
-void * funcion_hilo(void *estruc);
+void *funcion_hilo(void *estruc);
+void *impresionNumPalabras(void *estruc);
 
 typedef struct mi_estructuraTDA{
 	int linea_inicio;
@@ -18,6 +19,11 @@ typedef struct mi_estructuraTDA{
 	char *ruta;
 	char **palabras;
 }estructura;
+
+typedef struct palabrasVecesTDA{
+	char **palabras;
+	int *num_palabras;
+}
 
 int numero_lineas(char *ruta, int *tam_lineas){
 	if(ruta!=NULL){
@@ -51,7 +57,7 @@ void * funcion_hilo(void *estruc){
 	int *tam_lineas = datos->tam_lineas;
 	int *num_palabras = datos->num_palabras;
 	char *ruta = datos->ruta;
-	char **palabras = datos->*palabras;
+	char **palabras = datos->palabras;
 
 	//Open de archivo
 	FILE *fp = fopen(ruta,"r");
@@ -78,9 +84,9 @@ void * funcion_hilo(void *estruc){
 		palabra = strtok(linea,",.!?:;");
 		while(palabra!=NULL){
 			for(j=0;j<tPalabras;j++){
-				temp = palabras+j;
+				temp = *(palabras+j);
 				if(strcmp(temp,palabra)==0){
-					(num_palabras+j)+=1;
+					*(num_palabras+j)+=1;
 				}
 				palabra = strtok(NULL,",.!?:;");
 			}
@@ -90,14 +96,19 @@ void * funcion_hilo(void *estruc){
 	return (void *)suma;
 }
 
+void *impresionNumPalabras(void *estruc){
+
+}
+
 int main(int argc, char *argv[]){
 	if(argc<4){
 		return -1;
 	}
 	char* ruta = argv[1];
 	int nHilos = atoi(argv[2]); 
-	char** palabras;
 	int tPalabras=argc-3;
+	//Aquí habría que usa una estructura palabrasvecesTDA o sino solamente usar un arreglo global para las palabras y para el num de veces
+	char** palabras;
 	palabras = (char**)malloc(tPalabras*sizeof(char*));
 	int num_palabras[tPalabras];
 	int i;
@@ -110,7 +121,26 @@ int main(int argc, char *argv[]){
 	int nLineas;
 	int *tam_lineas;
 	nLineas = numero_lineas(ruta,tam_lineas);
-
+	
+	int ini,fin,div;
+	div = (int)nLineas/nHilos;
+	int j;
+	for(j=0;j<nHilos;i++){
+		ini=div*i;
+		if(i!=(nHilos-1))
+			fin=div*(i+1);
+		else
+			fin=nLineas;
+		//Creacion estructura
+		estructura *estruc = (estructura *)malloc(sizeof(estructura *));
+		estruc->linea_inicio = ini;
+		estruc->linea_final = fin;
+		estruc->tPalabras = tPalabras;
+		estruc->tam_lineas = tam_lineas;
+		estruc->num_palabras = num_palabras;
+		estruc->ruta = ruta;
+		estruc->palabras = palabras;
+	}
 
 	return 0;
 }
