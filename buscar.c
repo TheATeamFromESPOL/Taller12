56f76d0aefa1da29cd28b81a49a1da2065c97ba8
Luis Arízaga <luis.1142@hotmail.com>
2017-08-06 22:54:43 -0500

    huehue

--- buscar.c
+++ buscar.c
@@ -63,45 +63,64 @@ void * funcion_hilo(void *estruc){
 		posInicial += tam_lineas[i];
 	}
 
+	int j;
 	int posFinal = posInicial;
-	for(i=linea_inicio;i<=linea_final;i++){
-		posFinal += tam_lineas[i];
+	for(j=linea_inicio;j<=linea_final;j++){
+		posFinal += tam_lineas[j];
 	}
 
 	//Colocación de puntero en la posición correspondiente
 	fseek(fp,posInicial, SEEK_SET);
 	
-	char *linea = "";
-	char *palabra;
-	int j;
-	pthread_mutex_lock(&lock);
+	/*
 	while((ftell(fp)<=posFinal)&&(fgets(linea,MAX,fp)!=NULL)){
-		palabra = strtok(linea,",.!?:;");
+		palabra = strtok(linea," ,.!?:;");
 		while(palabra!=NULL){
 			printf("Palabra: %s",palabra);
 			for(j=0;j<tPalabras;j++){
+				pthread_mutex_lock(&lock);
 				if(strcmp(palabras[j],palabra)==0){
 					num_palabras[j]+=1;
  				}
- 				palabra = strtok(NULL,",.!?:;");
+				pthread_mutex_unlock(&lock);
+ 				palabra = strtok(NULL," ,.!?:;");
 			}
 		}
 	}
+	*/
 
+	pthread_mutex_lock(&lock);
+	char *linea = (char *)malloc(sizeof(char)*MAX);
+	char *palabra = "";
+	int k;
+	for(k = linea_inicio;k < linea_final;k++){
+		fgets(linea,MAX,fp);
+		palabra = strtok(linea," \n\e\t\\\?\f\'\"\v\b\r!^><·$%&/,()=º¡¢£¤¥¦§¨©ª«®¯|@#½¬-_ç`+*[]{}ḉç¿,.!?:;");
+		while(palabra!=NULL){
+			//printf("Palabra: %s\n",palabra);
+			int l;
+			for(l=0;l<tPalabras;l++){
+				//printf("Palabras[%i]: %s\n",l,palabras[l]);
+				if(strcmp(palabras[l],palabra)==0){
+					num_palabras[l] += 1;
+ 				}
+ 				palabra = strtok(NULL," \n\e\t\\\?\f\'\"\v\b\r!^><·$%&/,()=º¡¢£¤¥¦§¨©ª«®¯|@#½¬-_ç`+*[]{}ḉç¿,.!?:;");
+			}
+		}
+	}
 	pthread_mutex_unlock(&lock);
-
 	return (void *)0;
 }
 
 void *impresionNumPalabras(void * argumento){
 	while(1){
-		pthread_mutex_lock(&lock);
+		//pthread_mutex_lock(&lock);
 		int i;
 		printf("\nCantidad de veces que aparece cada palabra:\n");
 		for(i=0; i<tPalabras;i++){
 			printf("%s: %i veces\n",palabras[i],num_palabras[i]);
 		}
-		pthread_mutex_unlock(&lock);
+		//pthread_mutex_unlock(&lock);
 		sleep(1);
 	}
 	return (void *)0;
@@ -115,82 +134,72 @@ int main(int argc, char *argv[]){
     	}
 
 	if(argc<4){
+		printf("Cantidad de argumentos incorrecta");
 		return -1;
 	}
 
 	char* ruta = argv[1];
 	int nHilos = atoi(argv[2]);
-	printf("Argumentos: %i\n",argc);
 	tPalabras=argc-3;
-	printf("Palabras: %i\n",tPalabras);
-
-	//Aquí habría que usa una estructura palabrasvecesTDA o sino solamente usar un arreglo global para las palabras y para el num de veces --Opté por los arreglos globales
-	/*char** palabras;
-	palabras = (char**)malloc(tPalabras*sizeof(char*));
-	int num_palabras[tPalabras];*/
 
 	int i;
 	for(i = 0 ; i < tPalabras ; i++){
-		printf("Palabra a insertar: %s\n",argv[i+3]);
 		palabras[i]=argv[i+3];
 	}
-	/*for(i = 0 ; i < tPalabras ; i++){
-		printf("%s\n",palabras[i]);
-	}*/
+	
 	pthread_t *hilos;
 	hilos = (pthread_t*)malloc(nHilos*sizeof(pthread_t));
-	//printf("Crea los threads\n");
 
 	int *tam_lineas = (int *)malloc(MAX*sizeof(int));
-	//printf("Crea arreglo de tam_lineas\n");
 	int nLineas;
-	nLineas = numero_lineas(ruta,tam_lineas);//Sale violación de core
-	//printf("realiza la funcion numero_lineas\n");
-	//printf("numero de lineas del archivo:%i\n",nLineas);
-	for(i = 0 ; i < nLineas ; i++){
-		printf("linea=%i, valor:%i\n",i+1,tam_lineas[i]);
-	}
+	nLineas = numero_lineas(ruta,tam_lineas);
+
 	int ini,fin,div;
 	div = (int)nLineas/nHilos;
-	printf("%i",div);
 	int j;
 	//printf("funciona antes for\n");
 	for(j=0;j<nHilos;j++){
-		ini=div*i;
-		if(i!=(nHilos-1))
-			fin=div*(i+1);
+		ini=div*j;
+		if(j!=(nHilos-1))
+			fin=div*(j+1);
 		else
 			fin=nLineas;
+		//printf("Linea Inicio: %i   Linea Fin: %i",ini,fin);
 		//Creacion estructura
+		//printf("creacion estructura\n");
 		estructura *estruc = (estructura *)malloc(sizeof(estructura *));
 		estruc->linea_inicio = ini;
 		estruc->linea_final = fin;
 		estruc->tam_lineas = tam_lineas;
 		estruc->ruta = ruta;
-		int hilo = pthread_create(&hilos[i], NULL, funcion_hilo,(void *)estruc);
+		//printf("Se crea la estructura\n");
+		int hilo = pthread_create(&hilos[j], NULL, funcion_hilo,(void *)estruc);
 		if(hilo<0){
 			printf("Error creando hilo de conteo.");
 			return 0;
 		}
 	}
 	//printf("salio del for\n");
+	
 	pthread_t presentador;
 	int hiloPresentacion = pthread_create(&presentador, NULL, impresionNumPalabras, NULL);
 	if(hiloPresentacion<0){
 		printf("Error creando hilo de presentación.");
 		return 0;
 	}
+	
+	/*printf("Se hace los joins\n");
+	int hue;
+	for(hue=0;hue<nHilos;hue++){
+		pthread_join(hilos[hue],NULL);
+	}*/
+	//pthread_join(presentador,NULL);
 
-	for(j=0;j<nHilos;j++){
-		pthread_join(hilos[j],NULL);
-	}
-	pthread_join(hiloPresentacion,NULL);
-
-	printf("\nFINAL\nCantidad de veces que aparece cada palabra:\n");
+	/*printf("\nFINAL\nCantidad de veces que aparece cada palabra:\n");
 	pthread_join(hiloPresentacion,NULL);
 	for(i=0; i<tPalabras;i++){
 		printf("%s: %i veces\n",palabras[i],num_palabras[i]);
-	}
+	}*/
 
 	pthread_mutex_destroy(&lock);
 	return 0;
